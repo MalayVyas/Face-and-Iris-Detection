06e2369caabae72189e9d9398b6522d0ed1ecb93
MalayVyas <106895969+MalayVyas@users.noreply.github.com>
2023-01-21 13:54:53 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -1,22 +1,55 @@
-from streamlit_webrtc import webrtc_streamer, RTCConfiguration
-import av
+import numpy as np
 import cv2
+import streamlit as st
+faceCascade = cv2.CascadeClassifier(
+    'haarcascades/haarcascade_frontalface_default.xml')
+eyeCascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
+cap = cv2.VideoCapture(1)
+cap.set(3, 1920)
+cap.set(4, 1080)
+st.header("Face and Iris Detector")
+if st.button("Start Live Input"):
+    while True:
+        ret, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.3,
+            minNeighbors=5,
+            minSize=(30, 30)
+        )
 
-cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
 
-class VideoProcessor:
-	def recv(self, frame):
-		frm = frame.to_ndarray(format="bgr24")
+            eyes = eyeCascade.detectMultiScale(
+                roi_gray,
+                scaleFactor=1.5,
+                minNeighbors=10,
+                minSize=(5, 5),
+            )
 
-		faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey),
+                              (ex + ew, ey + eh), (0, 255, 0), 2)
+            cv2.imshow('Press Escape to exit.', img)
 
-		for x,y,w,h in faces:
-			cv2.rectangle(frm, (x,y), (x+w, y+h), (0,255,0), 3)
+        k = cv2.waitKey(1) & 0xff
+        if k == 27:
+            break
 
-		return av.VideoFrame.from_ndarray(frm, format='bgr24')
+    cap.release()
+    cv2.destroyAllWindows()
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
 
-webrtc_streamer(key="key", video_processor_factory=VideoProcessor,
-				rtc_configuration=RTCConfiguration(
-					{"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-					)
-	)
+while run:
+    _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('Stopped')
